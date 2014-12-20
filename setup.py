ee5a1e8d8c159cd67150f2007138bb44471ad77c
Ben Gamari <bgamari.foss@gmail.com>
2014-12-20 17:34:00 -0500

    Flesh out setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,12 @@ from setuptools import setup, find_packages
 setup(
     name = "squmfit",
     version = "0.1",
+    author = 'Ben Gamari',
+    author_email = 'ben@smart-cactus.org',
+    url = 'http://github.com/bgamari/squmfit',
+    license = 'BSD',
     packages = find_packages(),
+    install_requires=[
+        'numpy',
+    ],
 )
