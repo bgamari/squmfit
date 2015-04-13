76e62cf8058ddc8ba79e3210b5cfe2212a8b93c7
Ben Gamari <ben@smart-cactus.org>
2015-04-13 15:13:03 -0400

    plot: Whitespace cleanup

--- squmfit/plot.py
+++ squmfit/plot.py
@@ -32,12 +32,12 @@ def plot_fit(x, result, range=None, xscale='linear', errorbars=True, fig=None,
 
     if fig is None:
         fig = pl.figure()
-        
+
     if range is None:
         xmin = min(c.curve.user_args[x].min() for c in result.curves.values())
         xmax = max(c.curve.user_args[x].max() for c in result.curves.values())
         range = (xmin, xmax)
-        
+
     gs = gridspec.GridSpec(2,2, width_ratios=[3,1], height_ratios=[3,1])
     if with_residuals:
         ax_fits = pl.subplot(gs[0, 0])
@@ -48,7 +48,7 @@ def plot_fit(x, result, range=None, xscale='linear', errorbars=True, fig=None,
     else:
         ax_fits = pl.subplot(gs[0:1, 0])
         ax_residuals = None
-        
+
     ax_fits.set_xlim(range[0], range[1])
 
     for curve in result.curves.values():
@@ -56,7 +56,7 @@ def plot_fit(x, result, range=None, xscale='linear', errorbars=True, fig=None,
         if ax_residuals is not None:
             plot_curve_residuals(x, curve, xscale=xscale, axes=ax_residuals, abs_residuals=abs_residuals,
                                  c=pts_artist.lines[0].get_color())
-            
+
     ax_legend = gs[0:1, 1]
     ax_fits.legend(loc='upper left',
                    bbox_to_anchor=ax_legend.get_position(fig),
@@ -64,10 +64,10 @@ def plot_fit(x, result, range=None, xscale='linear', errorbars=True, fig=None,
                    mode='expand',
                    frameon=False,
                    **legend_kwargs)
-            
+
     pl.sca(ax_fits)
     return (ax_fits, ax_residuals)
-        
+
 def plot_curve(x, result, range=None, axes=None, npts=300,
                xscale='linear', errorbars=True, label=None, **kwargs):
     """
@@ -88,7 +88,7 @@ def plot_curve(x, result, range=None, axes=None, npts=300,
     xs = result.curve.user_args[x]
     if range is None:
         range = (xs[0], xs[-1])
-        
+
     if label is None:
         label = curve.name
 
