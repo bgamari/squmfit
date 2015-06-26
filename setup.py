c74cf89f9e9259521851b9e2d6b0611a3b77586a
Ben Gamari <ben@smart-cactus.org>
2015-06-26 09:57:58 +0200

    Drop Python 2 support

--- setup.py
+++ setup.py
@@ -28,9 +28,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
